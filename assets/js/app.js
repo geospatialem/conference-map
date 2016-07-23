c8f69abd254b275abffc93b66dc881324f577c1f
geospatialem <geospatialem@gmail.com>
2016-07-23 18:53:50 -0500

    Add a schedule + events
    Add a schedule modal dialog with a full list of conference activities and events. Add a new geojson 'events' that is specific to conference event host sites. Resolves #19.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var map, featureList, hotelSearch = [], attractionsSearch = [], establishmentsSearch = [], mqStreetBasemap = MQ.mapLayer();
+var map, featureList, hotelSearch = [], attractionsSearch = [], establishmentsSearch = [], eventsSearch = [], mqStreetBasemap = MQ.mapLayer();
 
 $(document).on("click", ".feature-row", function(e) {
   sidebarClick(parseInt($(this).attr("id"), 10));
@@ -6,6 +6,12 @@ $(document).on("click", ".feature-row", function(e) {
 
 $("#about-btn").click(function() {
   $("#aboutModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#schedule-btn").click(function() {
+  $("#scheduleModal").modal("show");
   $(".navbar-collapse.in").collapse("hide");
   return false;
 });
@@ -48,7 +54,7 @@ $("#sidebar-hide-btn").click(function() {
 });
 
 function sidebarClick(id) {
-  map.addLayer(hotelsLayer).addLayer(attractionsLayer).addLayer(establishmentsLayer);
+  map.addLayer(hotelsLayer).addLayer(attractionsLayer).addLayer(establishmentsLayer).addLayer(eventsLayer);
   var layer = markerClusters.getLayer(id);
   map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
   layer.fire("click");
@@ -593,6 +599,52 @@ $.getJSON("data/duluth/hotels.geojson", function (data) {
   map.addLayer(hotelsLayer);
 });
 
+var eventsLayer = L.geoJson(null);
+var events = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+    	icon: starMarker,
+    	title: feature.properties.NAME,
+    	riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" +
+      "<tr><th>Event Information</th><td>" + feature.properties.DETAILS + " (" + feature.properties.EVENTHOURS + ")</td></tr>" +
+      "<tr><th>Address</th><td>" + feature.properties.ADDRESS + "</td></tr>" +
+      "<tr><th>Full Hours</th><td>" + feature.properties.HOURS + "</td></tr>" +
+      "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + " (new window) <i class='fa fa-external-link-square'></i></a></td></tr><table>";
+      layer.on({
+        click: function (e) {
+	      $("#feature-title").html(feature.properties.NAME);
+          $("#feature-info").html(content);
+          $("#featureModal").modal("show");
+          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+            stroke: false,
+            fillColor: "#00FFFF",
+            fillOpacity: 0.7,
+            radius: 10
+          }));
+        }
+      });
+      $("#feature-list tbody").append('<tr class="feature-row" id="'+L.stamp(layer)+'"><td style="vertical-align: middle;"><span class="fa-stack"><i class="fa fa-square fa-stack-2x" style="color: #B74448;"></i><i class="fa fa-star fa-stack-1x" style="color: white;"></i></span></td><td class="feature-name">'+layer.feature.properties.NAME+'</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      eventsSearch.push({
+        name: layer.feature.properties.NAME,
+        address: layer.feature.properties.ADDRESS,
+        source: "Events",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+$.getJSON("data/duluth/events.geojson", function (data) {
+  events.addData(data);
+  map.addLayer(eventsLayer);
+});
+
 var attractionsLayer = L.geoJson(null);
 var attractions = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
@@ -711,11 +763,17 @@ map.on("overlayadd", function(e) {
   if (e.layer === establishmentsLayer) {
     markerClusters.addLayer(establishments);
   }
+  if (e.layer === eventsLayer) {
+    markerClusters.addLayer(events);
+  }
 });
 
 map.on("overlayremove", function(e) {
   if (e.layer === attractionsLayer) {
     markerClusters.removeLayer(attractions);
+  }
+  if (e.layer === eventsLayer) {
+    markerClusters.removeLayer(events);
   }
   if (e.layer === hotelsLayer) {
 	    markerClusters.removeLayer(hotels);
@@ -800,8 +858,9 @@ var groupedOverlays = {
   "Places of Interest": {
 		  "&nbsp;5k Fun Run/Walk Route": funRunWalkRoute,
 		  "<span class='fa-stack fa-lg'><i class='fa fa-square fa-stack-2x' style='color: #406573;'></i><i class='fa fa-bed fa-stack-1x' style='color: white;'></i></span>&nbsp;Hotels": hotelsLayer,
+      "<span class='fa-stack fa-lg'><i class='fa fa-square fa-stack-2x' style='color: #B74448;'></i><i class='fa fa-star fa-stack-1x' style='color: white;'></i></span>&nbsp;Conference Events": eventsLayer,
 		  "<span class='fa-stack fa-lg'><i class='fa fa-square fa-stack-2x' style='color: #72AF26;'></i><i class='fa fa-binoculars fa-stack-1x' style='color: white;'></i></span>&nbsp;Attractions": attractionsLayer,
-		  "<span class='fa-stack fa-lg'><i class='fa fa-square fa-stack-2x' style='color: #EB902E;'></i><i class='fa fa-cutlery fa-stack-1x' style='color: white;'></i></span>&nbsp;Establishments": establishmentsLayer,
+		  "<span class='fa-stack fa-lg'><i class='fa fa-square fa-stack-2x' style='color: #EB902E;'></i><i class='fa fa-cutlery fa-stack-1x' style='color: white;'></i></span>&nbsp;Establishments": establishmentsLayer
   }
 };
 
@@ -839,6 +898,16 @@ $(document).one("ajaxStop", function () {
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     local: attractionsSearch,
+    limit: 10
+  });
+
+  var eventsBH = new Bloodhound({
+    name: "Events",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: eventsSearch,
     limit: 10
   });
 
@@ -892,9 +961,13 @@ $(document).one("ajaxStop", function () {
     },
     limit: 10
   });
+
+
   attractionsBH.initialize();
-  hotelsBH.initialize();
   establishmentsBH.initialize();
+  eventsBH.initialize();
+  hotelsBH.initialize();
+
   geonamesBH.initialize();
 
   /* instantiate the typeahead UI */
@@ -911,6 +984,14 @@ $(document).one("ajaxStop", function () {
     	suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
     }
 	  }, {
+      name: "Events",
+      displayKey: "name",
+      source: eventsBH.ttAdapter(),
+      templates: {
+      	header: "<h4 class='typeahead-header'><span class='fa-stack'><i class='fa fa-square fa-stack-2x' style='color: #B74448;'></i><i class='fa fa-star fa-stack-1x' style='color: white;'></i></span>&nbsp;Events</h4>",
+      	suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
+      }
+  	  },{
 	    name: "Hotels",
 	    displayKey: "name",
 	    source: hotelsBH.ttAdapter(),
@@ -955,6 +1036,15 @@ $(document).one("ajaxStop", function () {
     if (datum.source === "Establishments") {
       if (!map.hasLayer(establishmentsLayer)) {
         map.addLayer(establishmentsLayer);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+    if (datum.source === "Events") {
+      if (!map.hasLayer(eventsLayer)) {
+        map.addLayer(eventsLayer);
       }
       map.setView([datum.lat, datum.lng], 17);
       if (map._layers[datum.id]) {
